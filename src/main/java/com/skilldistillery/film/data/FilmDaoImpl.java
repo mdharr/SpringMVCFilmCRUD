0e8acdf37edb144e59b1c231f355447a2646e9bc
Michael Harrington <mdharr7@gmail.com>
2022-12-11 03:16:47 +0800

    Add updateFilm method to FilmDaoImpl

--- src/main/java/com/skilldistillery/film/data/FilmDaoImpl.java
+++ src/main/java/com/skilldistillery/film/data/FilmDaoImpl.java
@@ -235,8 +235,36 @@ public class FilmDaoImpl implements FilmDAO {
 
 	@Override
 	public boolean updateFilm(Film film) {
-		// TODO Auto-generated method stub
-		return false;
+		Connection conn = null;
+		  try {
+		    conn = DriverManager.getConnection(URL, USER, PWD);
+		    conn.setAutoCommit(false); // START TRANSACTION
+		    String sql = "UPDATE film SET title=?, description=?, release_year=?, language_id=?, rental_duration=?, rental_rate=?, length=?, replacement_cost=?, rating=?, special_features=? "
+		               + " WHERE id=?";
+		    PreparedStatement stmt = conn.prepareStatement(sql);
+			stmt.setString(1, film.getTitle());
+			stmt.setString(2, film.getDescription());
+			stmt.setInt(3, film.getReleaseYear());
+			stmt.setInt(4, film.getLanguageId());
+			stmt.setInt(5, film.getRentalDuration());
+			stmt.setDouble(6, film.getRentalRate());
+			stmt.setInt(7, film.getLength());
+			stmt.setDouble(8, film.getReplacementCost());
+			stmt.setString(9, film.getRating());
+			stmt.setString(10, film.getSpecialFeatures());
+		    stmt.executeUpdate();
+
+		  } catch (SQLException sqle) {
+		    sqle.printStackTrace();
+		    if (conn != null) {
+		      try { conn.rollback(); } // ROLLBACK TRANSACTION ON ERROR
+		      catch (SQLException sqle2) {
+		        System.err.println("Error trying to rollback");
+		      }
+		    }
+		    return false;
+		  }
+		  return true;
 	}
 
 	@Override
